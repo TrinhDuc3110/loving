991066340734a2dbcd8f4b1fd1e55f79ec8dc442
GlobalDuc <96855379+GlobalDuc@users.noreply.github.com>
2022-12-30 23:04:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import Lover from './pages/Intro/Lover';
 import Calendar from './pages/Calendar/Calendar';
 import LoveCalendar from './pages/LoveCalendar/LoveCalendar';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPATgR-wOk7AbDgEzl0YoFpmZCKaT14vI",
+  authDomain: "loving-d1bac.firebaseapp.com",
+  projectId: "loving-d1bac",
+  storageBucket: "loving-d1bac.appspot.com",
+  messagingSenderId: "418815781612",
+  appId: "1:418815781612:web:9f28b50d52f65ccad453c9",
+  measurementId: "G-XSEY5YRNTH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <BrowserRouter>
